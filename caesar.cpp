641c0d1aa19fb6f9ceaeb455180610ddd4733f82
async0 <68186799+async0@users.noreply.github.com>
2020-12-07 17:11:49 +0300

    Update caesar.cpp

--- caesar.cpp
+++ caesar.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+    system("cat src/banner.txt");
+
     int i, y;
     char str[100];
 
@@ -12,9 +14,17 @@ int main()
     cout << "enter slide value" << endl;
     cin >> y;
          
-    for(i = 0; (i < 100 && str[i] != '\0'); i++)
-    str[i] = str[i] + y; 
-    cout << "\nEncrypted string: " << str << endl;
+    if (y < 14)
+    {
+    	for(i = 0; (i < 100 && str[i] != '\0'); i++)
+    	str[i] = str[i] + y; 
+    	cout << "\nEncrypted string: " << str << endl;
+    }
+    else
+    {
+	cout << "Slide value should not be higher than 13." << endl;
+    }    
+
 
     return 0;
 }
